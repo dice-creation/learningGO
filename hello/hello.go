0fc419adbfe1a2f8bdeb9482a8ede1ac6a4bb6ad
dice <alanzhang616@gmail.com>
2026-08-06 01:46:14 -0700

    feat: added spanish and french language hello

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
-func Hello(name string) string {
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
     if name == "" {
         name = "World"
+    }
+    if language == spanish {
+        return spanishHelloPrefix + name
+    }
+    if language == french {
+        return frenchHelloPrefix + name
     }
     return englishHelloPrefix + name
 }
 
 func main() {
-    fmt.Println(Hello("Jack"))
+    fmt.Println(Hello("Jack", ""))
 }
