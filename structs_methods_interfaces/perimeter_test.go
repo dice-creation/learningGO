0e3c43cd87f2c8822e4ae030cd64d3cbed15984a
dice <alanzhang616@gmail.com>
2026-08-08 13:42:04 -0700

    Table driven tests

--- structs_methods_interfaces/perimeter_test.go
+++ structs_methods_interfaces/perimeter_test.go
@@ -36,4 +36,25 @@ func TestArea(t *testing.T){
 
 		checkArea(t, circle, want)
 	})
+
+	t.Run("Table driven tests", func (t *testing.T){
+		areaTests := []struct {
+			name string
+			shape Shape
+			hasArea float64
+		}{
+			{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+			{name: "Cricle" ,shape: Circle{10}, hasArea: 314.1592653589793},
+			{name: "Triangle", shape: Triangle{12,6}, hasArea: 36.0},
+		}
+
+		for _, tt := range areaTests {
+			t.Run(tt.name, func(* testing.T){
+				got := tt.shape.Area()
+				if got != tt.hasArea {
+					t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+				}
+			})
+		}
+	})
 }
