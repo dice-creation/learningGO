d1053e83fc796565a09a9b3ca61fdeb1f668dc28
dice <alanzhang616@gmail.com>
2026-08-10 22:06:57 -0700

    using maps with errors

---
+++ 7. maps/dictionary.go
@@ -0,0 +1,18 @@
+package dictionary
+
+import "errors"
+
+type Dictionary map[string]string
+
+// func Search (dictionary map[string]string, word string) string{
+// return dictionary[word]
+// }
+var ErrNotFound = errors.New("could not find the word you were looking for") 
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
---
+++ 7. maps/go.mod
@@ -0,0 +1,3 @@
+module example/dictionary
+
+go 1.26.5
