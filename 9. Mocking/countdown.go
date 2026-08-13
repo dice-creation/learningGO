0aa0fbc5134a4eaefd730b10ea45a9f670166ef3
dice <alanzhang616@gmail.com>
2026-08-13 00:25:18 -0700

    refactor to mock real write and sleep

--- 9. Mocking/countdown.go
+++ 9. Mocking/countdown.go
@@ -41,3 +41,20 @@ type DefaultSleeper struct{}
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
+
+type SpyCountdownOperations struct {
+	Calls [] string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error){
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
